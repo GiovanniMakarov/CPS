a26effd9b908ff71087c8555d709c0b661dd7752
GiovanniMakarov <>
2022-10-22 18:24:04 +0300

    Bag fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import '../scss/style.scss'; 
 
-// ------------ Modals
 const asideMenuButtonOpen = document.querySelector('.burger-menu-open');
 const asideMenu = document.querySelector('.aside__container--menu');
 
@@ -20,7 +19,7 @@ asideMenu.addEventListener('click', event => {
 
     if ((event.target.classList.contains('call-button')) || 
          event.target.classList.contains('chat-button')) {
-            closeModal(asideMenu, 'false');
+            closeModal(asideMenu, false);
     };
 });
 
@@ -33,47 +32,44 @@ const asideCallback = document.querySelector('.aside__container--modal.callback'
 const buttonCallbackClose = document.querySelector('.callback-close');
 const blurBlock = document.querySelector('.blur-wrapper');
 
-buttonCallbackOpen.forEach(function(button) {
-    button.addEventListener('click', function() {
+buttonCallbackOpen.forEach((button) => {
+    button.addEventListener('click', () => {
         openModal(asideCallback);
+    
         blurBlock.addEventListener('click', () => closeModal(asideCallback));
         buttonCallbackClose.addEventListener('click', () => closeModal(asideCallback));
     })
 });
 
-buttonFeedbackOpen.forEach(function(button) {
-    button.addEventListener('click', function() {
+buttonFeedbackOpen.forEach((button) => {
+    button.addEventListener('click', () => {
         openModal(asideFeedback);
         blurBlock.addEventListener('click', () => closeModal(asideFeedback));
         buttonFeedbackClose.addEventListener('click', () => closeModal(asideFeedback));
     })
 });
 
-function closeModal (block, hideBlur = 'true') {
-    block.classList.remove('show');
-    if (hideBlur === 'true') {
-        blurBlock.classList.remove('show');
+const closeModal = (block, hideBlur = true) => {
+    block.classList.remove('show-slow');
+
+    if (hideBlur) {
+        blurBlock.classList.remove('show-slow');
         document.body.classList.remove('no-scroll');
     }
 }
 
-function openModal (block) {
-    block.classList.add('show');
-    blurBlock.classList.add('show');
+const openModal = (block) => {
+    block.classList.add('show-slow');
+    blurBlock.classList.add('show-slow');
     document.body.classList.add('no-scroll');
-    console.log(document.body.classList);
 }
 
-
-
-
-// ------------ Readmore buttons 
 
 const readMoreButtonServices = document.querySelector('.button-readmore--services');
 const readMoreButtonBrands = document.querySelector('.button-readmore--brands');
 const readMoreButtonEquipment = document.querySelector('.button-readmore--equipment');
 
-readMoreButtonServices.addEventListener('click', function() {
+readMoreButtonServices.addEventListener('click', () => {
     const activeDescription = document.querySelector('.services__content__description__text.active');
     if ( ! activeDescription ) return;
     
@@ -94,7 +90,7 @@ readMoreButtonEquipment.addEventListener ('click', () => {
 })
 
 
-function updateReadMoreButton (blockClass, buttonClass) {
+const updateReadMoreButton = (blockClass, buttonClass) => {
     const contentBlock = document.querySelector(blockClass);
     const readMoreButton = document.querySelector(buttonClass);
     if ( ! contentBlock) return;
@@ -109,10 +105,6 @@ function updateReadMoreButton (blockClass, buttonClass) {
     }
 }
 
-
-
-// ------------ Swiper
-
 const sliderBrands = document.querySelector('.swiper__brands');
 const sliderEquipment = document.querySelector('.swiper__equipment');
 const sliderPrice = document.querySelector('.swiper__price');
@@ -123,13 +115,13 @@ mobileSlider(sliderBrands, ".swiper__brands");
 mobileSlider(sliderEquipment,".swiper__equipment");
 mobileSlider(sliderPrice, ".swiper__price");
 
-window.addEventListener('resize', function () {
+window.addEventListener('resize', () => {
     mobileSlider(sliderBrands, ".swiper__brands");
     mobileSlider(sliderEquipment, ".swiper__equipment");
     mobileSlider(sliderPrice, ".swiper__price");
 })
 
-function mobileSlider(slider, swiperClass) {
+const mobileSlider = (slider, swiperClass) => {
     if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
         let swiper = new Swiper(swiperClass, {
             slidesPerView: "auto",
@@ -152,21 +144,16 @@ function mobileSlider(slider, swiperClass) {
     }
 }
 
-
-
-
-// ------------ Tabs
-
 const tabsButton = document.querySelectorAll('.services__slider__radiobtn')
 const tabsItems = document.querySelectorAll('.services__content__description__text');
 
-tabsButton.forEach(function(item) {
-    item.addEventListener('click', function() {
+tabsButton.forEach((item) => {
+    item.addEventListener('click', () => {
         const tabID = item.getAttribute("data-tab");
         const currentTab = document.querySelector(tabID);
 
         if ( ! currentTab.classList.contains('active')) {
-            tabsItems.forEach(function(item) {
+            tabsItems.forEach( (item) => {
                 item.classList.remove('active');
             })
 
